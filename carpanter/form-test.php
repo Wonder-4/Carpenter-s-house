e1ffb93233da9e291bc9966e089dd8531e91533c
黃翰隆 <issac11235813@gmail.com>
2016-05-19 10:35:46 +0800

    form-test
    test

---
+++ carpanter/form-test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML5>
+<html>
+    <title>Test-Form</title>
+    <body>
+        <form method="POST" action="/recieve" enctype="multipart/form-data">
+            <input name="guestbook_name" type="text"></input>
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <button value="submit"></button>
+        </form>    
+    </body>    
+</html>
---
+++ carpanter/recieve.php
@@ -0,0 +1,7 @@
+<?
+$gue=$_POST['guestbook_name'];
+$content=$_POST['content'];
+echo $gue;
+echo $content;
+
+?>
